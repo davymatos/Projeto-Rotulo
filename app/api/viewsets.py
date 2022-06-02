9337d32cbaeec117a37479f3f96604cbf8f2e8af
Davy de Matos <davymatos01@gmail.com>
2022-06-02 11:16:36 -0300

    Filtragem e Pesquisa
    Filtragem e Pesquisa

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework import viewsets
 from app.api import serializers
 from app import models
@@ -9,3 +11,6 @@ class CategoriasViewSet(viewsets.ModelViewSet):
 class IngredientesViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.IngredientesSerializer
     queryset = models.Ingredientes.objects.all()
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['nome_en', 'nome_alt', 'nome_br']
+    search_fields = ['nome_en', 'nome_alt', 'nome_br']
